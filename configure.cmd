04de63a0090902293d7d46a14dcf0ce0034af6a7
KO Myung-Hun <komh78@gmail.com>
2025-02-09 00:14:54 +0900

    my: configure.cmd: prepend /@unixroot to prefix
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -5,5 +5,8 @@ d=$(dirname "$0" | tr '\\' /)
 n=configure
 test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
 
-opts=""
+opts="
+    --prefix=/@unixroot/usr/local
+"
+
 "$d/$n" $opts "$@" 2>&1 | tee "$n.log"
